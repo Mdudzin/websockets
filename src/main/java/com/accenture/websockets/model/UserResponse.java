0e07bf6dbd27bca36c4855474fd0e8b6bc9ce017
mdudzin <maciej.dudzin@gmail.com>
2021-04-13 13:28:13 +0200

    small refactor

--- src/main/java/com/accenture/websockets/model/UserResponse.java
+++ src/main/java/com/accenture/websockets/model/UserResponse.java
@@ -2,9 +2,11 @@ package com.accenture.websockets.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
 
     private String content;
